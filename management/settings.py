2b7d6a1150ccf0165bbfcf30ec85ac30fc092a2e
hosam <semryhosam@gmail.com>
2026-02-12 15:26:12 +0200

    Remove default user from database configuration

--- management/settings.py
+++ management/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv('POSTGRES_NAME'),
-        "USER": os.getenv('POSTGRES_USER', 'postgres'),
+        "USER": os.getenv('POSTGRES_USER'),
         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "HOST": os.getenv('POSTGRES_HOST'),
         "PORT": os.getenv('POSTGRES_PORT'),
