3e47ec7add20e7f3053690f7f74dbe978318b461
hosam <semryhosam@gmail.com>
2026-02-13 11:52:50 +0200

    Add accounts app to installed apps and set auth user model to user model in accounts app

--- management/settings.py
+++ management/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'clubs',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "accounts.User"
