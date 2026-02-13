91973c91a4381bd8c4c07b66cfa1a6b42539a7ce
hosam <semryhosam@gmail.com>
2026-02-13 21:10:43 +0200

    Add rest framework and jwt to settings file

--- management/settings.py
+++ management/settings.py
@@ -30,9 +30,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.simplejwt',
     'clubs',
     'accounts',
 ]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
