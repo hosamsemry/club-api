52c38a76ad9f57697d82332e34851d97bf3aa732
hosam <semryhosam@gmail.com>
2026-02-14 14:16:50 +0200

    Add RolePermission class for role-based access control

---
+++ core/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class RolePermission(BasePermission):
+    def has_permission(self, request, view):
+        required_roles = getattr(view, "required_roles", [])
+        return (
+            request.user.is_authenticated and
+            request.user.role in required_roles
+        )
