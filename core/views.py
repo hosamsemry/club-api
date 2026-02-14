a4c3f8c35b5a44ee8a64a2f2f25c8710bcccacfc
hosam <semryhosam@gmail.com>
2026-02-14 14:16:59 +0200

    Implement TenantModelViewSet with authentication and club filtering

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class TenantModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return super().get_queryset().none()
+        return super().get_queryset().filter(
+            club=self.request.user.club
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(club=self.request.user.club)
+
+    def perform_update(self, serializer):
+        serializer.save(club=self.request.user.club)
