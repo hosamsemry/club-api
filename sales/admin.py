1d3040a31aad72d353f55e2404cb453709472055
hosam <semryhosam@gmail.com>
2026-03-13 00:43:34 +0200

    Register Sale model in Django admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Sale, SaleItem
 
-# Register your models here.
+
+class SaleItemInline(admin.TabularInline):
+    model = SaleItem
+    extra = 0
+    readonly_fields = ("subtotal",)
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_by", "total_amount", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("created_by__username",)
+    inlines = [SaleItemInline]
