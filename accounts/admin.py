a76c7d152c8b64dad5405bfe8740f4d1c140ab08
hosam <semryhosam@gmail.com>
2026-03-13 00:42:43 +0200

    Register User model in Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "club", "role", "is_active", "is_staff", "is_superuser")
+    list_filter = ("role", "is_active", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    ordering = ("email",)
