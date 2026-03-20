69b3c31dd3d6f070c9d237381a955d16c22fceff
hosam <semryhosam@gmail.com>
2026-03-20 20:28:39 +0200

    fix: import timezone from Django instead of Python’s time module

--- clubs/models.py
+++ clubs/models.py
@@ -1,5 +1,5 @@
-from time import timezone
 from django.db import models
+from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.crypto import get_random_string
 
