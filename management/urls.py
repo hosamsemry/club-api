640b2e38d1ba24d22af460b6e29148fe5dfb254d
hosam <semryhosam@gmail.com>
2026-03-21 14:44:14 +0200

    fix: update token authentication to use custom email-based view

--- management/urls.py
+++ management/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+from accounts.views import EmailTokenObtainPairView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
     path('api/core/', include('core.urls')),
