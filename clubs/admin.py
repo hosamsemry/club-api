71ecb2fcee35240f73233de682898217a88d5dd5
hosam <semryhosam@gmail.com>
2026-04-09 23:55:10 +0200

    feat: remove SubscriptionPlan model and related fields from ClubAdmin

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Club, SubscriptionPlan
+from .models import Club
 
 @admin.register(Club)
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "timezone", "subscription_plan", "is_active", "created_at")
+    list_display = ("name", "slug", "timezone", "is_active", "created_at")
     search_fields = ("name", "slug")
-    list_filter = ("is_active", "subscription_plan")
-
-@admin.register(SubscriptionPlan)
-class SubscriptionPlanAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "period", "created_at")
-    search_fields = ("name",)
-    list_filter = ("period",)
+    list_filter = ("is_active",)
